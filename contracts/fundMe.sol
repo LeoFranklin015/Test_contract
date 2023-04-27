031e39ae507fd2ab3ca70938f935672a0c77f907
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-04-27 22:37:23 +0530

    FUND

--- contracts/fundMe.sol
+++ contracts/fundMe.sol
@@ -2,26 +2,20 @@
 
 pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./priceConvertor.sol";
 
 contract FundMe {
+    using priceConvertor for uint256;
     uint256 public minamt = 50;
+    address[] public funders;
 
+    mapping(address => uint256) public addrtofund;
     function fundme() public payable 
     {
-        require(getconversionrate(msg.value) > minamt, "IT isn't enough!");
+        require(msg.value.getconversionrate() > minamt, "IT isn't enough!");
+        funders.push(msg.sender);
+        addrtofund[msg.sender]=msg.value;
     }
 
-    function getPrice() view public returns (uint256) 
-    {
-        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 ans,,,) = pricefeed.latestRoundData();
-        return uint256(ans * 1e8);
-    }
-
-    function getconversionrate(uint256 Ethamount) view public returns (uint256) 
-    {
-        uint256 ethprice = getPrice();
-        uint256 ETH_TOTAL = (ethprice*Ethamount) / 1e18;
-        return ETH_TOTAL;
-    }
+    
 }
