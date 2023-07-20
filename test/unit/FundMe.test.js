142f87f3968da0dac3357253bddd6bf1dd1cd0d6
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-20 17:38:51 +0530

    fund testing bug .

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,4 +1,4 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { deployments, ethers, getNamedAccounts } = require("hardhat");
 describe("FundMe", async () => {
   let fundMe;
@@ -7,13 +7,20 @@ describe("FundMe", async () => {
   beforeEach(async () => {
     deployer = (await getNamedAccounts()).deployer;
     await deployments.fixture(["all"]);
-    fundMe = ethers.getContractAt("FundMe", deployer);
-    mockV3Aggregator = ethers.getContractAt("MockV3Aggregator", deployer);
+    fundMe = await ethers.getContractAt("FundMe", deployer);
+    mockV3Aggregator = await ethers.getContractAt("MockV3Aggregator", deployer);
   });
   describe("Constructor", async () => {
     it("Sets the aggregator address correctly ", async () => {
       const response = await fundMe.priceFeed;
       assert.equal(response, mockV3Aggregator.address);
+    });
+  });
+  describe("fund", function () {
+    it("fails if you dont send enough eth", async function () {
+      await expect(fundMe.fund()).to.be.revertedWith(
+        "You need to spend more ETH!"
+      );
     });
   });
 });
